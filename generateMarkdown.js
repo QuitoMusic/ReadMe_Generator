52d679f94dafcd52ed64e0d484c092635d4008de
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-06-20 10:52:35 -0400

    Markdown function with layout

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,1 +1,46 @@
 
+// Generates markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${renderLicenseLink(data.license)}
+
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact me:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+
+${renderLicenseSection(data.license)}
+`;
+}
